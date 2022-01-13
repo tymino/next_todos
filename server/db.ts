05606abdf1c1519e46b52a6f75e856fcad30a2ca
tymino <d_team-lex@bk.ru>
2022-01-13 13:21:51 +0300

    update db

--- server/db.ts
+++ server/db.ts
@@ -3,8 +3,8 @@ import ITodo from '../types/db';
 class Store {
   private _store: ITodo[];
 
-  constructor(items: ITodo[]) {
-    this._store = [...items];
+  constructor() {
+    this._store = [];
   }
 
   getStore() {
@@ -46,36 +46,4 @@ class Store {
   }
 }
 
-export default new Store([
-  {
-    id: 'item-1',
-    isComplete: true,
-    content:
-      'Complete online JavaScript course Complete online JavaScript course Complete online JavaScript course',
-  },
-  {
-    id: 'item-2',
-    isComplete: false,
-    content: 'Jog around the park 3x',
-  },
-  {
-    id: 'item-3',
-    isComplete: false,
-    content: '10 minutes meditation',
-  },
-  {
-    id: 'item-4',
-    isComplete: false,
-    content: 'Read for 1 hour',
-  },
-  {
-    id: 'item-5',
-    isComplete: false,
-    content: 'Pick up groceries',
-  },
-  {
-    id: 'item-6',
-    isComplete: false,
-    content: 'complete Todo App on Frontend Mentor',
-  },
-]);
+export default new Store();
